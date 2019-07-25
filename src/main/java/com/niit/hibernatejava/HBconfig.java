8cf38798261d7a14779cb67aaa5898fcf06a089a
rameshritzz <53004901+rameshritzz@users.noreply.github.com>
2019-07-25 10:14:32 +0530

    Hibernate

---
+++ src/main/java/com/niit/hibernatejava/HBconfig.java
@@ -0,0 +1,14 @@
+package com.niit.hibernatejava;
+
+import java.util.Properties;
+
+import org.omg.CORBA.Environment;
+
+public class HBconfig {
+	
+public static SessionFactory createSessionFactory() {
+	Properties properties=new Properties();
+	properties.setProperty(Environment.
+	properties.setProperty(Environment., value)
+}
+}
